352118dca860e5186d4cccdd53caabbc5f9b81e5
Lashkari Hit <lashkarihit1813d@gmail.com>
2025-12-25 09:19:47 +0530

    add route name

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::post('register', [AuthController::class, 'register']);
+Route::post('register', [AuthController::class, 'register'])->name('register');
 
-Route::post('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
